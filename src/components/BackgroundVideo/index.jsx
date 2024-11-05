325b0a361be379231a803ba494740aed18553c33
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2024-11-05 00:00:50 -0300

    sv

--- src/components/BackgroundVideo/index.jsx
+++ src/components/BackgroundVideo/index.jsx
@@ -3,7 +3,7 @@ const BackgroundVideo = () => {
     <div className="fixed top-0 left-0 w-full h-full z-negative overflow-hidden">
       <video
         className="w-full h-full object-cover"
-        src="/assets/videos/Coffe.mp4"
+        src="/src/assets/videos/Coffe.mp4"
         loop
         muted
         autoPlay
